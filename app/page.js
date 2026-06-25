430f2a2f0ee6db261a343f184eddb8ed7acaea2e
Laren <pintolaren05@gmail.com>
2026-06-25 22:27:18 +0530

    feat: integrate layout components and toast notifications into the home page

--- app/page.js
+++ app/page.js
@@ -1,9 +1,17 @@
-import Image from "next/image";
+'use client'
+import BlogList from "@/Components/BlogList";
+import Footer from "@/Components/Footer";
+import Header from "@/Components/Header";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function Home() {
   return (
     <>
-
+      <ToastContainer theme="dark" />
+      <Header />
+      <BlogList />
+      <Footer />
     </>
-  );
+  )
 }
